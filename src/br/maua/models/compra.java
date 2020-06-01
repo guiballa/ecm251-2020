e155ea5365c08275680c074c0b1a24e51444a131
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-06-01 12:32:27 -0300

    Update compra.java
    implementação dos metodos e atributos do modelo

--- src/br/maua/models/compra.java
+++ src/br/maua/models/compra.java
@@ -1,4 +1,43 @@
 package br.maua.models;
 
+import java.util.Objects;
+
 public class compra {
+    private double valor;
+    private int ID;
+
+    public compra(double valor, int ID) {
+        this.valor = valor;
+        this.ID = ID;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "compra{" +
+                "valor=" + valor +
+                ", ID=" + ID +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        compra compra = (compra) o;
+        return Double.compare(compra.valor, valor) == 0 &&
+                ID == compra.ID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(valor, ID);
+    }
 }
