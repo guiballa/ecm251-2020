c4dac7f51d605eec733dad0cc215b07ca4725b8a
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-06-26 18:15:44 -0300

    Switch case menu

--- Prova P1/src/br/maua/system/System.java
+++ Prova P1/src/br/maua/system/System.java
@@ -1,7 +1,37 @@
 package br.maua.system;
 
-public class System {
-    public static void run(){
+import java.util.Scanner;
 
+public class System {
+    static private Scanner scanner;
+    public static void run() {
+        scanner = new Scanner(java.lang.System.in);
+        boolean executar = true;
+        int opcao;
+        do {
+            menu();
+            opcao = Integer.parseInt(scanner.nextLine());
+            switch (opcao) {
+                case 0:
+                    executar = false;
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 4:
+
+                    break;
+            }
+        }while(executar);
+    }
+    static private void menu() {
+        java.lang.System.out.println("Pizzaria o Rato que Ri:");
+        java.lang.System.out.println("1 - Nova venda");
+        java.lang.System.out.println("2 - Verificar pedidos");
+        java.lang.System.out.println("3 - Alterar pedidos");
+        java.lang.System.out.println("0 - Sair");
     }
 }
