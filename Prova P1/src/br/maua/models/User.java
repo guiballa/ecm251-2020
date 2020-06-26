9fba74a580217a708a6ea65d15f4c8b57658f7af
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-06-26 18:19:46 -0300

    Classe User com construtor

--- Prova P1/src/br/maua/models/User.java
+++ Prova P1/src/br/maua/models/User.java
@@ -1,4 +1,15 @@
 package br.maua.models;
 
 public class User {
+    private String nickname, password, email;
+
+    public User(String nickname, String password, String email) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
