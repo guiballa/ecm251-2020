53bd5a47bc6bc8562e47accfd61e872e68587eb7
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-06-26 18:28:18 -0300

    idGerado em pedidos

--- Prova P1/src/br/maua/models/Pedidos.java
+++ Prova P1/src/br/maua/models/Pedidos.java
@@ -1,4 +1,13 @@
 package br.maua.models;
 
+import java.util.Random;
+
 public class Pedidos {
+    private String geradorID(){
+        Random random = new Random();
+        String idGerado = "";
+        for(int i=0; i<3; i++)
+            idGerado += random.nextInt(10);
+        return idGerado;
+    }
 }
