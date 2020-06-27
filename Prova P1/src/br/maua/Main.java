f68f3a218311128b9eed6a2178affb858e0a6528
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-06-26 21:12:57 -0300

    Inicio do Javadoc

--- Prova P1/src/br/maua/Main.java
+++ Prova P1/src/br/maua/Main.java
@@ -1,6 +1,13 @@
 package br.maua;
 import br.maua.system.System;
 
+/**
+ * Classe concreta que representa o main do programa. Ela que vai chamar o sistema.
+ * @author Guilherme Ballardini - Guiballa@hotmail.com
+ *  *  * @since 26/06/2020
+ *  *  * @version 1.0
+ */
+
 public class Main {
     public static void main(String[] args) {
 	System.run();
--- Prova P1/src/br/maua/models/User.java
+++ Prova P1/src/br/maua/models/User.java
@@ -2,19 +2,29 @@ package br.maua.models;
 
 import br.maua.interfaces.AutenticarSenha;
 
+/**
+ * Classe concreta que representa um usuario. Ela define o que é um usuario e como se criar um.
+ * @author Guilherme Ballardini - Guiballa@hotmail.com
+ *  *  * @since 26/06/2020
+ *  *  * @version 1.0
+ */
+
 public class User implements AutenticarSenha {
     private String nickname, password, email;
 
+    /**
+     * Contrutor da classe User para que ela receba um nome, uma senha e um email todas as vezes que um usuario for criado.
+     */
     public User(String nickname, String password, String email) {
         this.nickname = nickname;
         this.password = password;
         this.email = email;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
+    /**
+     * Método implementado da interface AutenticarSenha, que ele checa se a senha passada é a cadastrada pelo usuario.
+     * @return Um boolean se a senha é a senha cadastrada ou não.
+     */
     @Override
     public boolean checkPassword(String senha) {
         return senha.equals(password);
